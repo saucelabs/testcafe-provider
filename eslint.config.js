a1984a96ba5705c7fafd43d959aa7b53ba5b2528
Mike Han <mike.han@saucelabs.com>
2024-02-20 11:09:36 -0700

    add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
 const eslint = require('@eslint/js');
-const tseslint = require('typescript-eslint');
+const typescriptEslint = require('typescript-eslint');
+const eslintConfigPrettier = require('eslint-config-prettier');
 
-module.exports = tseslint.config(
+module.exports = typescriptEslint.config(
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...typescriptEslint.configs.recommended,
+  ...eslintConfigPrettier,
 );
