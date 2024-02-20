aee89bf8b79810df616d4cf2b5c6cf8cf129ffb8
Mike Han <mike.han@saucelabs.com>
2024-02-20 12:38:01 -0700

    Bah, use the old config

--- eslint.config.js
+++
@@ -1,9 +0,0 @@
-const eslint = require('@eslint/js');
-const typescriptEslint = require('typescript-eslint');
-const eslintConfigPrettier = require('eslint-config-prettier');
-
-module.exports = typescriptEslint.config(
-  eslint.configs.recommended,
-  ...typescriptEslint.configs.recommended,
-  eslintConfigPrettier,
-);
