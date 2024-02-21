eef6d669e1204096be101de556a14e7a963368e0
Mike Han <mike.han@saucelabs.com>
2024-02-21 16:10:49 -0700

    apply prettier to test

--- test/e2e/change-elements-style.js
+++ test/e2e/change-elements-style.js
@@ -1,30 +1,31 @@
 import { ClientFunction, Selector } from 'testcafe';
 
-fixture('Change Element Style')
-    .page('https://devexpress.github.io/testcafe/example');
+fixture('Change Element Style').page(
+  'https://devexpress.github.io/testcafe/example',
+);
 
 test('Hide an element', async (t) => {
-    const populateButton     = Selector('#populate');
-    const hidePopulateButton = ClientFunction(() => {
-        document.getElementById('populate').style.display = 'none';
-    });
+  const populateButton = Selector('#populate');
+  const hidePopulateButton = ClientFunction(() => {
+    document.getElementById('populate').style.display = 'none';
+  });
 
-    await t.expect(populateButton.visible).ok();
+  await t.expect(populateButton.visible).ok();
 
-    await hidePopulateButton();
+  await hidePopulateButton();
 
-    await t.expect(populateButton.visible).notOk();
+  await t.expect(populateButton.visible).notOk();
 });
 
 test('Change header color', async (t) => {
-    const header            = Selector('h1');
-    const removeHeaderColor = ClientFunction(() => {
-        document.querySelector('h1').style.color = '#111';
-    });
+  const header = Selector('h1');
+  const removeHeaderColor = ClientFunction(() => {
+    document.querySelector('h1').style.color = '#111';
+  });
 
-    await t.expect(header.getStyleProperty('color')).eql('rgb(47, 164, 207)');
+  await t.expect(header.getStyleProperty('color')).eql('rgb(47, 164, 207)');
 
-    await removeHeaderColor();
+  await removeHeaderColor();
 
-    await t.expect(header.getStyleProperty('color')).eql('rgb(17, 17, 17)');
+  await t.expect(header.getStyleProperty('color')).eql('rgb(17, 17, 17)');
 });
