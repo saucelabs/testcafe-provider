d846f53fd7c53c38dd55e0049776cb0c02f23525
Alex Plischke <alex.plischke@saucelabs.com>
2024-03-12 15:31:22 -0700

    chore: update project setup

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+const ts = require('typescript-eslint');
+const js = require('@eslint/js');
+const prettier = require('eslint-config-prettier');
+
+module.exports = ts.config(
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  prettier,
+  {
+    ignores: ['lib/**'],
+  },
+  {
+    files: ['**/*.js'],
+    rules: {
+      '@typescript-eslint/no-var-requires': 'off',
+    },
+  },
+  {
+    files: ['tests/**/*.js'],
+    languageOptions: {
+      globals: {
+        document: true,
+      },
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        __dirname: true,
+        console: true,
+        exports: true,
+        module: true,
+        require: true,
+      },
+    },
+  },
+);
